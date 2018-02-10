36d12eabef4fbfc109a60b57b005cb28b1b390c2
Travis Johnson <travis.johnson@nwea.org>
2018-02-09 18:10:28 -0800

    init

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -10,16 +10,33 @@ using Microsoft.Extensions.Logging;
 
 namespace WebApplication1
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			BuildWebHost(args).Run();
+		}
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
-    }
+		public static IWebHost BuildWebHost(string[] args)
+		{
+			var builder = WebHost.CreateDefaultBuilder(args)
+					.UseStartup<Startup>();
+
+			string env = builder.GetSetting("environment");
+			var envMsg = "ASPNETCORE_ENVIRONMENT/Environment variable ";
+			if (string.IsNullOrWhiteSpace(env))
+				throw new ArgumentNullException("environment", envMsg + "missing!");
+			else
+				Console.WriteLine(envMsg + "found!");
+
+			string doSeed = builder.GetSetting("SeedDb");
+			var seedMsg = "SeedDb in appsettings.json ";
+			if (string.IsNullOrWhiteSpace(doSeed))
+				throw new ArgumentNullException("SeedDb", seedMsg + "missing!");
+			else
+				Console.WriteLine(seedMsg + "found!");
+
+			return builder.Build();
+		}
+	}
 }
